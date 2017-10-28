33014a31409b1f376fa7d48c79676d9d1eececa4
feliciabong <100075778@students.swinburne.edu.my>
2017-10-28 23:56:37 +0800

    fixed database connection issue for the signup feature

---
+++ db_init.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE useracc_;
+
+use useracc_;
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+	lastname VARCHAR(30) NOT NULL,	
+	email VARCHAR(50) NOT NULL UNIQUE KEY,
+	location VARCHAR(50),
+    username VARCHAR(255) NOT NULL UNIQUE KEY,
+    password VARCHAR(64) NOT NULL,
+	date TIMESTAMP
+);
