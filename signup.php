25401bf3234268d193e1c8824f5ac0c2aa2d3570
feliciabong <100075778@students.swinburne.edu.my>
2017-10-30 23:23:36 +0800

    added change account details form

--- signup.php
+++ signup.php
@@ -2,52 +2,19 @@
 <html>
   <head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <title>Deallo Craft House</title>
 
-    <title></title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/shop-homepage.css" rel="stylesheet">
+        <?php
+            include_once "./tag/header.php"
+        ?>
 
   </head>
 
   <body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#">Deallo Craft House</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About Us</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact Us</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="signup.html">Sign Up</a>
-			<li class="nav-item">
-              <a class="nav-link" href="login.html">Sign In</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+   <?php
+            include_once "./tag/Nbar.php"
+        ?>
       
       <div class="container pad">
       <div class= "row">
@@ -95,13 +62,9 @@
       </div>
       
 <!-- Footer -->
-    <footer class="py-5 bg-dark footer">
-      <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; DealloCraftHouse - eMarketPlace DP2 2017</p>
-      </div>
-      <!-- /.container -->
-    </footer>
-
+    <?php
+            include_once "./tag/footer.php"
+        ?>
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
