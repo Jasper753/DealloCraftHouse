5e219584de733e062a809c9f12c752ef5897826d
feliciabong <100075778@students.swinburne.edu.my>
2017-10-30 23:48:51 +0800

    fix minor layout issue

--- tag/footer.php
+++ tag/footer.php
@@ -1,16 +1,6 @@
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/shop-homepage.css" rel="stylesheet">
-<<<<<<< HEAD
-	
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-=======
->>>>>>> 65050a8c5f93809fc55c9212208ba5d9b3992d42
+    <footer class="py-5 bg-dark footer">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; DealloCraftHouse - eMarketPlace DP2 2017</p>
+      </div>
+      <!-- /.container -->
+    </footer>
